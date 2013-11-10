86020370520997572eb0d74bde2254016eb13272
Y-Deekshit-Reddy <y.deekshit.reddy@gmail.com>
2013-11-10 20:37:43 +0530

    botrandom.c

--- botrandom.c
+++ botrandom.c
@@ -1,1 +1,42 @@
+//a bot which plays at the first available place in the 9*9 grid
+#include <math.h>
+#include <gamio.h>
+int main()
+{
+    int *x;
+    int *y;
+     char**** mat4 = (char****) malloc(3*sizeof(char***));
+    int i,j,k,l;
+    for (i=0; i<3; i++) {
+        mat4[i] = (char***) malloc(3*sizeof(char**));
+        for (j=0; j<3; j++) {
+            mat4[i][j] = (char**) malloc(3*sizeof(char*));
+            for (k=0; k<3; k++) {
+                mat4[i][j][k] = (char*) malloc(3*sizeof(char));
 
+    mat4=input4D(x,y);
+    char play=toplayXorO(mat4);
+    if(x==-1)
+    {
+        bool dobreak = false;
+        for(i=0;!dobreak &&i<3;i++)
+        {
+            for(j=0;!dobreak &&j<3;j++)
+            {
+                for(k=0;!dobreak &&k<3;k++)
+                {
+                    for(l=0;!dobreak &&l<3;l++)
+                    {
+                        if(mat4[i][j][k][l]=='X'||mat4[i][j][k][l]=='O')
+                        else
+                        {
+                            mat4[i][j][k][l]==play;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
