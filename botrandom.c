2678aa0de8dc0f1d90eddfd439d79c65659e2ba3
Y-Deekshit-Reddy <y.deekshit.reddy@gmail.com>
2013-11-10 20:50:54 +0530

    botrandom.c

--- botrandom.c
+++ botrandom.c
@@ -1,6 +1,7 @@
 //a bot which plays at the first available place in the 9*9 grid
 #include <math.h>
 #include <gamio.h>
+char toplayXorO(char**** mat4);
 int main()
 {
     int *x;
@@ -38,5 +39,46 @@ int main()
             }
         }
     }
+    else
+    {
+        bool dobreak = false;
+        for(k=0;!dobreak &&k<3;k++)
+        {
+            for(l=0;!dobreak &&l<3;l++)
+            {
+                if(mat4[x][y][k][l]=='X'||mat4[x][y][k][l]=='O')
+                else
+                {
+                    mat4[x][y][k][l]==play;
+                    break;
+                }
+            }
+        }
+    }
+}
+char toplayXorO(char**** mat4)
+{
+    int i,j,k,l;
+    int c=0;
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            for(k=0;k<3;k++)
+            {
+                for(l=0;l<3;l++)
+                {
+                    if(mat4[i][j][k][l]=='X')
+                        c++;
+                    else if(mat4[i][j][k][l]=='O')
+                        c--;
+                }
+            }
+        }
+    }
+    if(c)
+        return 'O';
+    else
+        return 'X';
 
 }
